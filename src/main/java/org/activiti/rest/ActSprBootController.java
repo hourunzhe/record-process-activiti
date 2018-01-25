da3341e7f16dd525975b41b58052b71de5df6780
hou_runzhe <123789456>
2018-01-25 14:48:35 +0800

    将获取进度图片的逻辑改成了下载，并显示在浏览器中

--- src/main/java/org/activiti/rest/ActSprBootController.java
+++ src/main/java/org/activiti/rest/ActSprBootController.java
@@ -19,7 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLEncoder;
 import java.util.*;
 
 
@@ -60,16 +64,19 @@ public class ActSprBootController {
 
     @ResponseBody
     @RequestMapping(value = "/bpmn", method = RequestMethod.GET)
-    public void getBpmnModel() throws IOException {
+    public void getBpmnModel(HttpServletResponse response) throws IOException {
         List<ProcessDefinition> processDefinitions = repositoryService.createProcessDefinitionQuery()
                 .processDefinitionKey("recordProcess").list();
         int bytesWritten = 0;
         int byteCount = 0;
         byte[] bytes = new byte[1024];
-        @Cleanup OutputStream outputStream = new FileOutputStream("/home/hrz/Desktop/file-new.png");
+        @Cleanup OutputStream outputStream = response.getOutputStream();
+        response.setContentType("image/png");
+        response.setHeader("Content-disposition",
+                "inline;filename=" + URLEncoder.encode(new Date().getTime() + ".png", "utf-8"));
         String diagramResourceName = processDefinitions.get(0).getDiagramResourceName();
-        @Cleanup InputStream imageStream = repositoryService.getResourceAsStream(
-                processDefinitions.get(0).getDeploymentId(), diagramResourceName);
+        @Cleanup InputStream imageStream = repositoryService
+                .getResourceAsStream(processDefinitions.get(0).getDeploymentId(), diagramResourceName);
         while ((byteCount = imageStream.read(bytes)) != -1) {
             outputStream.write(bytes, bytesWritten, byteCount);
             outputStream.flush();
@@ -78,7 +85,7 @@ public class ActSprBootController {
 
     @ResponseBody
     @RequestMapping(value = "/bpmn/v2/{taskId}", method = RequestMethod.GET)
-    public void getPng(@PathVariable("taskId")String taskId) throws IOException {
+    public void getPng(@PathVariable("taskId") String taskId, HttpServletResponse response) throws IOException {
         Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
         BpmnModel bpmnModel = repositoryService.getBpmnModel(task.getProcessDefinitionId());
         // 经过的节点
@@ -89,7 +96,6 @@ public class ActSprBootController {
         for (HistoricActivityInstance hai : finishedInstances) {
             finishedActiveActivityIds.add(hai.getActivityId());
         }
-
         // 已完成的节点+当前节点
         activeActivityIds.addAll(finishedActiveActivityIds);
         activeActivityIds.addAll(runtimeService.getActiveActivityIds(task.getProcessInstanceId()));
@@ -104,7 +110,10 @@ public class ActSprBootController {
         int bytesWritten = 0;
         int byteCount = 0;
         byte[] bytes = new byte[1024];
-        @Cleanup OutputStream outputStream = new FileOutputStream("/home/hrz/Desktop/file-new2.png");
+        @Cleanup OutputStream outputStream = response.getOutputStream();
+        response.setContentType("image/png");
+        response.setHeader("Content-disposition",
+                "inline;filename=" + URLEncoder.encode(new Date().getTime() + ".png", "utf-8"));
         while ((byteCount = imageStream.read(bytes)) != -1) {
             outputStream.write(bytes, bytesWritten, byteCount);
             outputStream.flush();
@@ -113,9 +122,9 @@ public class ActSprBootController {
 
     private void getHighLightedFlows(Map<String, FlowElement> flowElements, List<String> highLightedFlows, List<String> activeActivityIds) {
         flowElements.forEach((s, flowElement) -> {
-            if(flowElement instanceof SequenceFlow
+            if (flowElement instanceof SequenceFlow
                     && activeActivityIds.contains(((SequenceFlow) flowElement).getSourceRef())
-                    && activeActivityIds.contains(((SequenceFlow) flowElement).getTargetRef())){
+                    && activeActivityIds.contains(((SequenceFlow) flowElement).getTargetRef())) {
                 highLightedFlows.add(s);
             }
         });
